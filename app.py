1d879ae1918cde04ae969d00284b15eb8dcbb1ef
KingKazmaa <114176897+KingKazmaa@users.noreply.github.com>
2023-09-21 01:58:58 -0500

    refactored and added barbarian class

--- app.py
+++ app.py
@@ -4,19 +4,9 @@ from dataclasses import dataclass
 import random
 
 
-class Start:
-    name = input(f'Welcome traveler! What is the name of our brave adventurer?  ')
-    if name.isdigit():
-        print('Haha nice try!')
-        name = input('I need a real name: ')
-    os.system('cls' if os.name == 'nt' else 'clear')
-    # FIND AN EASIER WAY TO GIVE PROMPTS THAN JUST SPAMMING PRINT
-    # MAYBE INSERT TXT FILES ?
-    print(f'Now let us partake on this perilous journey {name}!')
-
 class Character:
-    species = 'Human'
-    health: 100
+    name: str
+    health: int
     weapon: str
     mana: int
     weapon_damage: int
@@ -32,7 +22,7 @@ class Character:
     # REDUCES ENEMY HEALTH
     def attack():
         # enemy.health -= 5
-        print(f'{name} attacks using bare hands! it isn\'t very effective')
+        print(f'{Character.name} attacks using bare hands! it isn\'t very effective')
         
     def gain_xp():
         xp += 50
@@ -40,64 +30,112 @@ class Character:
         print(f'current xp: {xp}')
     
     def faint():
-        if health <= 0:
-            print(f'{name} has fainted...')
-            
+        if Character.health <= 0:
+            print(f'{Character.name} has fainted...')
+
+character = Character()
+
+class Enemy:
+    health: 100
+    weapon = 'dagger'
+    weapon_damage: 20
+    xp_gain: int
+    
+    def attack():
+        print(f'The enemy attacks! It does 20 damage')
+        Character.health -= 25
+        print(f'{Character.health}')
+    
+    def potion():
+        Enemy.health = 100 if Enemy.health >= 100
+        Enemy.health += 20
+        
+    def faint():
+        if Enemy.health <= 0:
+            print(f'enemy has fainted...')
+
+enemy = Enemy()
 
 class Mage(Character):
     health = 125
     mana = 150
-    dodge_chance: .10
+    dodge_chance = .10
     weapon = f'Magic Staff'
     weapon_damage = 15
     
     def fireball():
-        if mana <= 44:
-            print(f'You don't have enough mana to cast this!')
+        if Mage.mana <= 44:
+            print(f'You don\'t have enough mana to cast this!')
             # return to fight menu
-        # enemy.health -= 30
+        enemy.health -= 30
         mana -= 45
-        print(f'{name} casts Fireball!!')
+        print(f'{Mage.name} casts Fireball!!')
         print(f'It does 30 damage!')
 
     def heal():
-        health = 125 if health >= 125 else health += 25
-        print(f'You healed 25hp! Current hp is {health}')
+        Mage.health = 125 if Mage.health >= 125 else Mage.health + 25
+        print(f'You healed 25hp! Current hp is {Mage.health}')
     
     
     # GIVES A 10% CHANCE TO SAVE THE PLAYER FROM DYING
     def arise():
         revive_chance = random.randint(100)
         if revive_chance <= 10:
-            health = 125 * 0.5
-            mana = 150 * 0.5
+            Mage.health = 125 * 0.5
+            Mage.mana = 150 * 0.5
             # RETURN TO BATTLE SEQUENCE
-        
+mage = Mage()
 class Barbarian(Character):
-    pass
+    health = 180
+    mana = 75
+    weapon = f'Double Axe'
+    weapon_damage = 35
+    
+    def double_strike():
+        double_chance = random.randint(10)
+        if double_chance <= 7:
+            enemy.health -= Barbarian.weapon_damage * 2
+        else:
+            enemy.health -= Barbarian.weapon_damage
 
 class Archer(Character):
     pass
 
-class Enemy():
-    species: 'monster'
-    health: 100
-    weapon: 'axe'
-    weapon_damage: 25 # or int
+
+
+class Start:
+
+    def get_name():
+        name = input(f'Welcome traveler! What is the name of our brave adventurer?  ')
+        if name.isdigit():
+            print('Haha nice try!')
+            name = input('I need a real name: ')
+        return name
     
-    attack():
-        pass
-    
-    potion():
-        Enemy.health = 100 if Enemy.health >= 100
-        Enemy.health += 20
-        
-    faint():
-        True if Enemy.health <= 0:
-            Print(f'enemy has fainted')
-            character.gain_xp()
+    def get_char_type(): 
+        print(f'Now choose a character type:')
+        print(f'Barbarian -- Archer -- Mage')
+        char_type = input()
+        match char_type:
+            case "barbarian":
+                char_type = Barbarian()
+                print(f' You are {Start.name} the {char_type}')
+                
+            case "archer":
+                char_type = Archer()
+                print(f' You are {Start.name} the {char_type}')
+                
+            case "mage":
+                char_type = Mage()
+                print(f' You are {Start.name} the {char_type}')
+        return char_type
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-# class Fight():?
+    print(f'Now let us partake on this perilous journey {name}!')
+with open('intro.txt', 'r') as file:
+        content = file.read()
+        print(content)
+# class Fight:
 
 
-# class Main():
+# class Main:
